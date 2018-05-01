290d34fbf29d16016921ce00f26d9f2a48dd9adf
Wahid Ali <wahid.ali@psquickit.com>
2018-05-01 14:05:56 +0530

    commited by wahid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import {ErrorHandler, NgModule} from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+var versions = require('../environments/versions');
+
+console.log(versions.versions.revision);
+
 
 import { AppComponent } from './app.component';
 import * as Rollbar from 'rollbar';
 import {RollbarErrorHandler} from '../services/rollbar-error-handler';
+import { version } from 'punycode';
 
 const rollbarConfig = {
   accessToken: 'f627d5e044a24b9987a23e54c5df352e',
   captureUncaught: true,
   captureUnhandledRejections: true,
   enabled: true,
-  code_version: '1.0',
+  code_version: versions.versions.revision,
   environment: 'dev'
 };
 
