26ea393dc147f02661365fcaa1382e69bb3a228e
Wahid Ali <wahid.ali@psquickit.com>
2018-05-03 19:04:45 +0530

    committed by wahid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,15 +16,9 @@ const rollbarConfig = {
   accessToken: 'f627d5e044a24b9987a23e54c5df352e',
   captureUncaught: true,
   captureUnhandledRejections: true,
-  source_map_enabled: true,
-  enabled: true,
+    enabled: true,
   code_version: versions.versions.revision,
-  environment: 'production',
-  server: {
-          branch: 'master',
-          host: 's3.us-east-2.amazonaws.com',
-          root: '/'
-        }
+  environment: 'production'
 };
 
 console.log(JSON.stringify(rollbarConfig));
