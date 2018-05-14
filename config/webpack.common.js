ac49de716e62f7a060c109c0f4913291a81973aa
Wahid Ali <wahid.ali@psquickit.com>
2018-05-14 18:48:28 +0530

    Added bug

---
+++ config/webpack.common.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var helpers = require('./helpers');
+
+module.exports = {
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'main': './src/main.ts'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: ['ts-loader'],
+        exclude: [/\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/]
+      }
+    ]
+  },
+
+  plugins: [
+    // Workaround for angular/angular#11580
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /angular(\\|\/)core(\\|\/)@angular/,
+      helpers.root('./src'), // location of your src
+      {} // a map of your routes
+    ),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+   
+
+    new webpack.SourceMapDevToolPlugin({
+      filename: "[file].map",
+      exclude: ["vendor.js"]
+    })
+  ]
+};
--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: false
     },
     angularCli: {
       environment: 'dev'
